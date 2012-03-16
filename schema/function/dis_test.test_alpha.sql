4625ec29899388f047a0cb5c7f892d043246b14e
jobywalker <joby@imperium.org>
2012-03-16 14:19:15 -0700

    Example test

---
+++ schema/function/dis_test.test_alpha.sql
@@ -0,0 +1,49 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = dis_test, pg_catalog;
+
+--
+-- Name: test_alpha(); Type: FUNCTION; Schema: dis_test; Owner: postgres
+--
+
+CREATE OR REPLACE FUNCTION test_alpha() RETURNS void
+    LANGUAGE plpgsql
+    VOLATILE
+    SECURITY INVOKER
+    AS $_$
+-- module: example
+-- submodule: basic
+-- plan: 3
+DECLARE
+    _scores   dis.score[] := '{}'::dis.score[];
+BEGIN
+    _scores := ARRAY[
+        dis.assert(TRUE, 'This is a test'),
+        dis.assert(FALSE, 'Oops this will fail'),
+        dis.assert(TRUE, 'This also works')
+    ];
+    PERFORM dis.tally(_scores);
+END;
+$_$;
+
+ALTER FUNCTION dis_test.test_alpha() OWNER TO postgres;
+
+--
+-- Name: FUNCTION test_alpha(); Type: COMMENT; Schema: dis_test; Owner: postgres
+--
+
+COMMENT ON FUNCTION test_alpha() IS 'Demo test (2012-03-15)';
+
+
+--
+-- PostgreSQL database dump complete
+--
