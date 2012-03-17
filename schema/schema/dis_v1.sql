da69770e23f3862f1abbb520b1c3932e1add9c1c
jobywalker <joby@imperium.org>
2012-03-16 21:24:50 -0700

    XML report of a test result

---
+++ schema/schema/dis_v1.sql
@@ -0,0 +1,33 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+--
+-- Name: dis_v1; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA dis_v1;
+
+
+ALTER SCHEMA dis_v1 OWNER TO postgres;
+
+--
+-- Name: dis_v1; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA dis_v1 FROM PUBLIC;
+REVOKE ALL ON SCHEMA dis_v1 FROM postgres;
+GRANT ALL ON SCHEMA dis_v1 TO postgres;
+GRANT ALL ON SCHEMA dis_v1 TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
